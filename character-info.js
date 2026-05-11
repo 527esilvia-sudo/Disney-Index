771cdaeaaa64e4812b56fca82477743d9384e305
527esilvia-sudo <527esilvia@frhsd.com>
2026-05-11 23:52:18 +0000

    this is gonna take forever- js not linked yet

---
+++ character-info.js
@@ -0,0 +1,151 @@
+const movieBank = [
+    {
+        movie: "Snow White and the Seven Dwarfs",
+        characters: [
+            {
+                cname: "Snow White",
+                role: "Protagonist",
+                personality: "Kind",
+                species: "Human"
+            },
+            {
+                cname: "The Evil Queen",
+                role: "Villain",
+                personality: "Vain",
+                species: "Human"
+            },
+            {
+                cname: "Magic Mirror",
+                role: "Villain Asset",
+                personality: "Truthful",
+                species: "Enchanted Object"
+            },
+            {
+                cname: "Prince Florian",
+                role: "Love Interest",
+                personality: "Charming",
+                species: "Human"
+            },
+            {
+                cname: "Doc",
+                role: "Side Character",
+                personality: "Smart",
+                species: "Dwarf"
+            },
+            {
+                cname: "Grumpy",
+                role: "Side Character",
+                personality: "Stubborn",
+                species: "Dwarf"
+            },
+            {
+                cname: "Happy",
+                role: "Side Character",
+                personality: "Cheerful",
+                species: "Dwarf"
+            },
+            {
+                cname: "Sleepy",
+                role: "Side Character",
+                personality: "Tired",
+                species: "Dwarf"
+            },
+            {
+                cname: "Bashful",
+                role: "Side Character",
+                personality: "Shy",
+                species: "Dwarf"
+            },
+            {
+                cname: "Sneezy",
+                role: "Side Character",
+                personality: "Clumsy",
+                species: "Dwarf"
+            },
+            {
+                cname: "Dopey",
+                role: "Side Character",
+                personality: "Silly",
+                species: "Dwarf"
+            },
+            {
+                cname: "The Hunter",
+                role: "Assistant to Villain",
+                personality: "Honorable",
+                species: "Human"
+            }
+        ]
+    },
+
+    {
+        movie: "The Lion King",
+        characters: [
+            {
+                cname: "Simba",
+                role: "Protagonist",
+                personality: "Brave",
+                species: "Lion"
+            },
+            {
+                cname: "Nala",
+                role: "Co-Protagonist",
+                personality: "Strong",
+                species: "Lion"
+            },
+            {
+                cname: "Mufasa",
+                role: "Mentor",
+                personality: "Wise",
+                species: "Lion"
+            },
+            {
+                cname: "Scar",
+                role: "Villain",
+                personality: "Cunning",
+                species: "Lion"
+            },
+            {
+                cname: "Zazu",
+                role: "Advisor",
+                personality: "Loyal",
+                species: "Hornbill"
+            },
+            {
+                cname: "Rafiki",
+                role: "Guide",
+                personality: "Wise",
+                species: "Mandrill"
+            },
+            {
+                cname: "Timon",
+                role: "Sidekick",
+                personality: "Funny",
+                species: "Meerkat"
+            },
+            {
+                cname: "Pumbaa",
+                role: "Sidekick",
+                personality: "Carefree",
+                species: "Warthog"
+            },
+            {
+                cname: "Shenzi",
+                role: "Villain Group",
+                personality: "Sly",
+                species: "Hyena"
+            },
+            {
+                cname: "Banzai",
+                role: "Villain Group",
+                personality: "Aggressive",
+                species: "Hyena"
+            },
+            {
+                cname: "Ed",
+                role: "Villain Group",
+                personality: "Chaotic",
+                species: "Hyena"
+            }
+        ]
+    }
+];
