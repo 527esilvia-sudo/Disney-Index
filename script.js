f1c61b10fe423db7a2eed458bdb59a960354e075
527esilvia-sudo <527Esilvia@frhsd.com>
2026-05-11 11:57:10 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const movieBank = [
+    {
+        movie: [],
+        cname: [],
+        role: [],
+        personality: [],
+        genre: []
+    },
+
+    {
+        movie: 'Snow White and the Seven Dwarfs',
+        cname: 'Snow White',
+        role: 'Protagonist',
+        personality: 'Kind',
+        genre: 'Fairy Tale'
+    },
+       {
+        movie: 'Snow White and the Seven Dwarfs',
+        cname: 'Snow White',
+        role: 'Protagonist',
+        personality: 'Kind',
+        genre: 'Fairy Tale'
+    },
+
+
+]
